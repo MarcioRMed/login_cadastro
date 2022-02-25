1e67fc38ac04396365ee3c0e604350180cb308e6
Márcio Ramos Medeiros <80805761+MarcioRMed@users.noreply.github.com>
2022-02-25 19:31:16 -0300

    ajust

--- js/cadastro.js
+++ js/cadastro.js
@@ -2,8 +2,84 @@
 //ver senha 
 
 let btn = document.querySelector('#verSenha')
+let btnConfirmSenha = document.querySelector('#verConfirmSenha')
 
-btn.addEventListener('click', ()=>{  
+let nome = document.querySelector('#nome')
+let labelNome = document.querySelector('#labelNome')
+
+let usuario = document.querySelector('#usuario')
+let labelUsuario = document.querySelector('#labelUsuario')
+
+let senha = document.querySelector('#senha')
+let labelSenha = document.querySelector('#labelSenha')
+
+let confirmSenha = document.querySelector('#confirmSenha')
+let labelConfirmSenha = document.querySelector('#labelConfirmSenha')
+
+
+nome.addEventListener('keyup' , () => {
+  if(nome.value.length <= 2 ){
+    labelNome.setAttribute('style', 'color: red')
+    labelNome.innerHTML = 'Nome *Insira no mínimo 3 caracteres'
+    nome.setAttribute('style', 'border-color: red')
+  }else{
+    labelNome.setAttribute('style' , 'color: green')
+    labelNome.innerHTML = 'Nome'
+    nome.setAttribute('style', 'border-color: green')
+  }
+})
+
+usuario.addEventListener('keyup' , () => {
+  if(usuario.value.length <= 4 ){
+    labelUsuario.setAttribute('style', 'color: red')
+    labelUsuario.innerHTML = 'Usuário *Insira no mínimo 5 caracteres'
+    usuario.setAttribute('style', 'border-color: red')
+  }else{
+    labelUsuario.setAttribute('style' , 'color: green')
+    labelUsuario.innerHTML = 'Usuário'
+    nome.setAttribute('style', 'border-color: green')
+  }
+})
+
+senha.addEventListener('keyup' , () => {
+  if(senha.value.length <= 5 ){
+    labelSenha.setAttribute('style', 'color: red')
+    labelSenha.innerHTML = 'Senha *Insira no mínimo 6 caracteres'
+    senha.setAttribute('style', 'border-color: red')
+  }else{
+    labelSenha.setAttribute('style' , 'color: green')
+    labelSenha.innerHTML = 'Senha'
+    senha.setAttribute('style', 'border-color: green')
+  }
+})
+
+
+confirmSenha.addEventListener('keyup' , () => {
+  if(senha.value != confirmSenha.value){
+    labelConfirmSenha.setAttribute('style', 'color: red')
+    labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
+    confirmSenha.setAttribute('style', 'border-color: red')
+  }else{
+    labelConfirmSenha.setAttribute('style' , 'color: green')
+    labelConfirmSenha.innerHTML = 'Confirmar Senha'
+    confirmSenha.setAttribute('style', 'border-color: green')
+  }
+})
+
+
+
+
+function cadastrar(){
+  
+
+
+}
+
+
+
+
+
+btn.addEventListener('click', ()=>{ 
 
   let inputSenha = document.querySelector('#senha')
 
@@ -14,15 +90,11 @@ btn.addEventListener('click', ()=>{
   }
 })
 
-
 // confirm senha
-
-
-let btnConfirmSenha = document.querySelector('#verConfirmSenha')
 
 btnConfirmSenha.addEventListener('click', ()=>{  
 
-  let inputConfirmSenha = document.querySelector('#ConfirmSenha')
+  let inputConfirmSenha = document.querySelector('#confirmSenha')
 
   if(inputConfirmSenha.getAttribute('type') == 'password'){
     inputConfirmSenha.setAttribute('type', 'text')
